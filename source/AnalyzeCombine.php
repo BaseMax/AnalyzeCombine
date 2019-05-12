2fef342ae730cb2a8d773b12b8afc3b83d4f5c3c
Max Base <MaxBaseCode@Gmail.Com>
2019-05-12 17:00:04 +0430

    Update AnalyzeCombine.php

--- source/AnalyzeCombine.php
+++ source/AnalyzeCombine.php
@@ -1,1 +1,10 @@
-
+<?php
+/**
+*
+* @Name : AnalyzeCombine.php
+* @Version : 1.0
+* @Programmer : Max
+* @Date : 2019-05-12
+* @Released under : https://github.com/BaseMax/ChessWeb/blob/master/LICENSE
+* @Repository : https://github.com/BaseMax/ChessWeb
+*/
